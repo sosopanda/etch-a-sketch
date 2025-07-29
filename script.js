2b64ccd18d7cfe29609ea2bd52da6767881a82b0
sosopanda <mcamarasantacreu@gmail.com>
2025-07-29 15:03:50 +0200

    added adjustable resolution controled some exceptions  and fancied CSS

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 const container = document.querySelector("#container");
+const res = document.querySelector("#res");
+let resValue = 16;
 
 let divs = []
+let size = 0;
 
 for(let i = 0 ; i < 256 ; i++){
     divs.push(document.createElement("div"));
     divs.at(i).addEventListener("mouseover", function (e) {
         e.target.style.background = "grey";
     });
+
+    size = 800/(resValue);
+
+    divs.at(i).setAttribute("style","width:"+size+"px; height:"+size+"px");
+
     container.appendChild(divs.at(i));
 }
+
+res.addEventListener("click",() => {
+
+    for(let i = 0 ; i < resValue*resValue ; i++){
+        divs.push(document.createElement("div"));
+        divs.at(i).remove();
+    }
+    
+    do{
+
+        resValue = prompt("Please introduce a resolution (Max 100): ","64");
+
+    } while(resValue > 100 || resValue < 1)
+
+    for(let i = 0 ; i < resValue*resValue ; i++){
+        divs.push(document.createElement("div"));
+        divs.at(i).addEventListener("mouseover", function (e) {
+            e.target.style.background = "grey";
+        });
+
+        size = 800/(resValue);
+
+        divs.at(i).setAttribute("style","width:"+size+"px; height:"+size+"px");
+
+        container.appendChild(divs.at(i));
+    }
+});
